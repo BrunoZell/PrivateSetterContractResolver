fc86a224c10ce85b8b07549616a06c20eb7a2226
Bruno Zell <bruno.zzell@gmail.com>
2017-12-04 16:47:13 +0100

    Cleaned up namespaces in test models

--- PrivateSetterContractResolver.Tests/Models/DerivedPrivateProperties.cs
+++ PrivateSetterContractResolver.Tests/Models/DerivedPrivateProperties.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PrivateSetterContractResolver.Tests.Models
-{
+﻿namespace PrivateSetterContractResolver.Tests.Models {
     class DerivedPrivateProperties : BasePrivateProperties
     {
         public DerivedPrivateProperties(string notDerived, string getterSetter, string getterOnly)
